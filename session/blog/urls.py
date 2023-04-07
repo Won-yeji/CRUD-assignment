49b6897c24d873947bed027c924cf13de81e1e85
Won-yeji <wyj3326@naver.com>
2023-04-07 15:53:34 +0900

    crudfinal

--- session/blog/urls.py
+++ session/blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('blog/<int:blog_id>',views.detail,name='detail'),
     path('blog/new',views.new,name='new'),
     path('blog/create',views.create,name='create'),
+    path('blog/edit/<int:blog_id>', views.edit, name='edit'),
+    path('blog/update/<int:blog_id>', views.update, name='update'),
+    path('bolg/delete/<int:blog_id>', views.delete, name='delete'),
+    path('blog/comment/<int:blog_id>', views.add_comment_to_post, name='add_comment_to_post'),
 ]
--- session/blog/views.py
+++ session/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Blog
+from .forms import BlogForm
+from .forms import CommentForm
 
 def home(request):
     blogs = Blog.objects.all()
@@ -18,3 +20,57 @@ def create(request):
     new_blog.content = request.POST['content']
     new_blog.save()
     return redirect('detail',new_blog.id)
+
+#def create(request):
+#    form = BlogForm(request.POST)
+
+   # 착한 사용자
+#    if form.is_vaild():
+#        new_blog = form.save(commit=False)
+#        new_blog.save()
+#        return redirect('detail', new_blog.id)
+    
+    # 나쁜 사용자
+#    return render(request, "new.html")
+
+def edit(request, blog_id):
+    edit_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'edit.html',{'edit_blog':edit_blog})
+
+def update(request, blog_id):
+    old_blog = get_object_or_404(Blog, pk=blog_id)
+    old_blog.title = request.POST['title']
+    old_blog.content = request.POST['content']
+    old_blog.save()
+    return redirect('detail', old_blog.id)
+
+# def update(request, blod_id):
+#    old_blog = get_object_or_404(Blog, pk=blog_id)
+#    form = BlogForm(request.POST, instance=old_blog)
+
+    # 착한 사용자
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.save()
+        return redirect('detail', old_blog.id)
+    
+    # 나쁜 사용자
+    return redirect(request, "new.html")
+
+def delete(request, blog_id):
+    delete_blog = get_object_or_404(Blog, pk=blog_id)
+    delete_blog.delete()
+    return redirect('home')
+
+def add_comment_to_post(request, blog_id):
+    blog=get_object_or_404(Blog, pk=blog_id)
+    if request.method == "GET":
+        form=CommentForm(request.GET)
+        if form.is_valid():
+            comment=form.save(commit=False)
+            comment.post=blog
+            comment.save()
+            return redirect('detail', blog_id)
+        else:
+            form=CommentForm()
+        return render(request, 'add_comment_to_post.html', {'form':form})
